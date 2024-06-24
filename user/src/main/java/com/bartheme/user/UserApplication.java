fcc88ed130079b47380c25779b63572e05d20053
kubabartecki <169511@stud.prz.edu.pl>
2024-06-24 20:38:17 +0200

    init user service

---
+++ user/src/main/java/com/bartheme/user/UserApplication.java
@@ -0,0 +1,13 @@
+package com.bartheme.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserApplication.class, args);
+    }
+
+}
