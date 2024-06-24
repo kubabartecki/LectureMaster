9d1c6dd8348f8d88b9c5077e94d4cdc8979deeb4
kubabartecki <169511@stud.prz.edu.pl>
2024-06-24 20:06:29 +0200

    add roles and permissions enums

---
+++ security/src/main/java/com/bartheme/security/model/ApplicationUserRole.java
@@ -0,0 +1,33 @@
+package com.bartheme.security.model;
+
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static com.bartheme.security.model.ApplicationUserPermission.*;
+
+public enum ApplicationUserRole {
+    STUDENT(new HashSet<>()),
+    TEACHER(new HashSet<>(Arrays.asList(LECTURE_READ, LECTURE_WRITE, STUDENT_READ, STUDENT_WRITE)));
+
+    private final Set<ApplicationUserPermission> permissions;
+
+    ApplicationUserRole(Set<ApplicationUserPermission> permissions) {
+        this.permissions = permissions;
+    }
+
+    public Set<ApplicationUserPermission> getPermissions() {
+        return permissions;
+    }
+
+    public Set<SimpleGrantedAuthority> getGrantedAuthorities() {
+        Set<SimpleGrantedAuthority> permissions = getPermissions().stream()
+                .map(permission -> new SimpleGrantedAuthority(permission.getPermission()))
+                .collect(Collectors.toSet());
+        permissions.add(new SimpleGrantedAuthority("ROLE_" + name()));
+        return permissions;
+    }
+}
